a8b9b271ee941f6702dfb4027fd7f703b2b24929
Pedro Lorandi <hello@pedrolorandi.com>
2022-12-14 21:53:56 -0500

    Add functions to append tweets dynamically in the index

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,38 +6,54 @@
 
 $(document).ready(function () {
 
-  const createTweetElement = function (tweets) {
-    let $tweet = `
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }  
+  }
+
+  const createTweetElement = function (tweet) {
+    return `
     <article class="tweet">
       <header>
-        <img src="${tweets.user.avatars}" alt="User picture">
-        <span>${tweets.user.name}</span>
+        <img src="${tweet.user.avatars}" alt="User picture">
+        <span>${tweet.user.name}</span>
       </header>
-      <p>${tweets.content.text}</p>
+      <p>${tweet.content.text}</p>
       <footer>
-        <span class="date">${tweets.created_at}</span>
+        <span class="date">${tweet.created_at}</span>
         <a href="#"><i class="fa-solid fa-flag" alt="Flag"></i></a>
         <a href="#"><i class="fa-sharp fa-solid fa-repeat" alt="Repeat"></i></a>
         <a href="#"><i class="fa-solid fa-heart" alt="Like"></i></a>
       </footer>
     </article>
-    `
-    return $tweet;
+    `;
   }
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
-
-  const $tweet = createTweetElement(tweetData);
-
-  $('#')
+  renderTweets(data);
 });
